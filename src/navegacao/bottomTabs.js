96c1c027131cae037e7cd7ddaf3f593c1a50edaa
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2026-03-05 22:45:54 -0300

    stack ok

--- src/navegacao/bottomTabs.js
+++ src/navegacao/bottomTabs.js
@@ -1,5 +1,8 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import AntDesign from '@expo/vector-icons/AntDesign';
+
 import StackExemplos from './stackExemplos';
 import StackAtividades from './stackAtividades';
 
@@ -9,15 +12,19 @@ function BottomTabs() {
 
     return (
         <Tab.Navigator
-            screenOptions={{
-                headerStyle: { backgroundColor: '#ff0' },
-                // headerShown: false,   
-                tabBarActiveTintColor: 'green',
-                // tabBarInactiveTintColor: 'orange', 
-                // tabBarShowLabel: false, 
-                // tabBarLabelStyle: { display: 'none' }                 
-                // tabBarLabelPosition: 'below-icon'                
-            }}
+            screenOptions={
+                {
+                    tabBarActiveTintColor: 'tomato',
+                    tabBarInactiveTintColor: 'gray',
+                    headerStyle: { backgroundColor: '#ff0' },
+                    // headerShown: false,   
+                    // tabBarActiveTintColor: 'green',
+                    // tabBarInactiveTintColor: 'orange', 
+                    // tabBarShowLabel: false, 
+                    // tabBarLabelStyle: { display: 'none' }                 
+                    // tabBarLabelPosition: 'below-icon'
+                }
+            }
         // initialRouteName="Home"
         >
             <Tab.Screen
@@ -29,15 +36,31 @@ function BottomTabs() {
                     //     fontSize: 16,
                     //     fontFamily: 'Georgia',
                     //     fontWeight: 300,
-                    // },
+                    // },                    
+                    tabBarIcon: ({ focused }) => {
+                        // You can return any component that you like here!
+                        const color = focused ? 'tomato' : 'gray';
+                        return <MaterialCommunityIcons name="notebook-outline" size={24} color={color} />;
+                    },
+
                 }}
 
+
             />
-            <Tab.Screen 
-                name="StackAtividades" 
-                component={StackAtividades}  
+            <Tab.Screen
+                name="StackAtividades"
+                component={StackAtividades}
+                options={
+                    {
+                        tabBarIcon: ({ focused }) => {
+                            // You can return any component that you like here!
+                            const color = focused ? 'tomato' : 'gray';
+                            return <MaterialCommunityIcons name="notebook-edit-outline" size={24} color={color} />;
+                        },
+                    }
+                }
             />
-        </Tab.Navigator>
+        </Tab.Navigator >
     );
 }
 
